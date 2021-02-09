8a154b9d514b11571a87ef36fbd0fff1c9e24757
AndrewLEEJM <woals214@gmail.com>
2021-02-09 22:19:21 +0900

    Chapter3

--- src/test/java/com/jpatest/jpatest/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jpatest/jpatest/domain/posts/PostsRepositoryTest.java
@@ -2,16 +2,17 @@ package com.jpatest.jpatest.domain.posts;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PostsRepositoryTest {
 	
@@ -38,5 +39,23 @@ public class PostsRepositoryTest {
 		Posts posts = postsList.get(0);
 		assertThat(posts.getTitle()).isEqualTo(title);
 		assertThat(posts.getContent()).isEqualTo(content);
+	}
+	
+	@Test
+	public void BaseTimeEntity_등록() {
+		//given
+		LocalDateTime now = LocalDateTime.of(2021, 2, 9, 0, 0, 0);
+		postRepository.save(Posts.builder().title("title").content("content").author("woals@gmail.com").build());
+		
+		//when
+		List<Posts> postsList = postRepository.findAll();
+		
+		//then
+		Posts posts = postsList.get(0);
+		
+		System.out.println(">>> createDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+		
+		assertThat(posts.getCreatedDate()).isAfter(now);
+		assertThat(posts.getModifiedDate()).isAfter(now);
 	}
 }
