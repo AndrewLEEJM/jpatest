47eec9e37a04c5f181439b37d14fad8e9c58f3fb
KR620619 <KR620619@amway.com>
2021-02-16 09:54:27 +0900

    구글 로그인 구현

--- src/main/java/com/jpatest/jpatest/web/IndexController.java
+++ src/main/java/com/jpatest/jpatest/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.jpatest.jpatest.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.jpatest.jpatest.config.auth.dto.SessionUser;
 import com.jpatest.jpatest.service.PostsService;
 import com.jpatest.jpatest.web.dto.PostsResponseDto;
 
@@ -15,10 +18,17 @@ import lombok.RequiredArgsConstructor;
 public class IndexController {
 	
 	private final PostsService postsService;
+	private final HttpSession httpSession;
 
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("posts", postsService.findAllDesc());
+		SessionUser user = (SessionUser) httpSession.getAttribute("user");
+		
+		if(user != null) {
+			model.addAttribute("userName", user.getName());
+		}
+		
 		return "index";
 	}
 	
@@ -26,6 +36,7 @@ public class IndexController {
 	public String postsSave() {
 		return "posts-save";
 	}
+	
 	@GetMapping("/posts/update/{id}")
 	public String postsUpdate(@PathVariable Long id, Model model) {
 		PostsResponseDto dto = postsService.findById(id);
